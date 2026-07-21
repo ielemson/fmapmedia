7bef22599bafae47f7c435ff86b8eaa737bdc7a1
ielemson <ielemson@gmail.com>
2026-07-21 17:14:16 +0100

    updated og graph for magazine

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,78 +7,175 @@
 
     @php
         $siteName = setting('site_name', 'FutureMap Media');
+
         $pageTitle = trim($__env->yieldContent('title'));
-        $title = $pageTitle ? $pageTitle . ' | ' . $siteName : $siteName;
+        $title = $pageTitle
+            ? $pageTitle . ' | ' . $siteName
+            : $siteName;
 
-        $defaultDescription = setting('meta_description', 'FutureMap Media provides news, magazines, articles and insightful media content.');
-        $defaultKeywords = setting('meta_keywords', 'FutureMap Media, News, Magazine, Articles');
+        $defaultDescription = setting(
+            'meta_description',
+            'FutureMap Media provides news, magazines, articles and insightful media content.'
+        );
 
-        $description = trim($__env->yieldContent('meta_description', $defaultDescription));
-        $keywords = trim($__env->yieldContent('meta_keywords', $defaultKeywords));
+        $defaultKeywords = setting(
+            'meta_keywords',
+            'FutureMap Media, News, Magazine, Articles'
+        );
 
-        $canonicalUrl = trim($__env->yieldContent('canonical_url', url()->current()));
+        $description = trim(
+            $__env->yieldContent('meta_description', $defaultDescription)
+        );
 
-        $ogType = trim($__env->yieldContent('og_type', 'website'));
-        $ogTitle = trim($__env->yieldContent('og_title', $title));
-        $ogDescription = trim($__env->yieldContent('og_description', $description));
+        $keywords = trim(
+            $__env->yieldContent('meta_keywords', $defaultKeywords)
+        );
 
-        $defaultImage = setting_asset(setting('logo'), 'frontend/assets/images/logo.png');
-        $sharedImage = trim($__env->yieldContent('og_image', $defaultImage));
+        $canonicalUrl = trim(
+            $__env->yieldContent('canonical_url', url()->current())
+        );
 
-        if ($sharedImage && !str_starts_with($sharedImage, 'http://') && !str_starts_with($sharedImage, 'https://')) {
+        /*
+        |--------------------------------------------------------------------------
+        | Open Graph Settings
+        |--------------------------------------------------------------------------
+        */
+
+        $ogType = trim(
+            $__env->yieldContent('og_type', 'website')
+        );
+
+        $ogUrl = trim(
+            $__env->yieldContent('og_url', $canonicalUrl)
+        );
+
+        $ogTitle = trim(
+            $__env->yieldContent('og_title', $title)
+        );
+
+        $ogDescription = trim(
+            $__env->yieldContent('og_description', $description)
+        );
+
+        $defaultImage = setting_asset(
+            setting('logo'),
+            'frontend/assets/images/logo.png'
+        );
+
+        $sharedImage = trim(
+            $__env->yieldContent('og_image', $defaultImage)
+        );
+
+        if (
+            $sharedImage &&
+            !str_starts_with($sharedImage, 'http://') &&
+            !str_starts_with($sharedImage, 'https://')
+        ) {
             $sharedImage = url('/' . ltrim($sharedImage, '/'));
         }
 
-        $ogImageAlt = trim($__env->yieldContent('og_image_alt', $ogTitle));
-        $ogImageType = trim($__env->yieldContent('og_image_type', ''));
-        $ogImageWidth = trim($__env->yieldContent('og_image_width', ''));
-        $ogImageHeight = trim($__env->yieldContent('og_image_height', ''));
+        $secureImage = trim(
+            $__env->yieldContent('og_image_secure_url', $sharedImage)
+        );
 
-        $twitterCard = trim($__env->yieldContent('twitter_card', 'summary_large_image'));
-        $twitterTitle = trim($__env->yieldContent('twitter_title', $ogTitle));
-        $twitterDescription = trim($__env->yieldContent('twitter_description', $ogDescription));
-        $twitterImage = trim($__env->yieldContent('twitter_image', $sharedImage));
-        $twitterImageAlt = trim($__env->yieldContent('twitter_image_alt', $ogImageAlt));
+        if (
+            $secureImage &&
+            !str_starts_with($secureImage, 'http://') &&
+            !str_starts_with($secureImage, 'https://')
+        ) {
+            $secureImage = url('/' . ltrim($secureImage, '/'));
+        }
 
-        if ($twitterImage && !str_starts_with($twitterImage, 'http://') && !str_starts_with($twitterImage, 'https://')) {
+        $ogImageAlt = trim(
+            $__env->yieldContent('og_image_alt', $ogTitle)
+        );
+
+        $ogImageType = trim(
+            $__env->yieldContent('og_image_type', 'image/jpeg')
+        );
+
+        $ogImageWidth = trim(
+            $__env->yieldContent('og_image_width', '1200')
+        );
+
+        $ogImageHeight = trim(
+            $__env->yieldContent('og_image_height', '630')
+        );
+
+        /*
+        |--------------------------------------------------------------------------
+        | Twitter / X Settings
+        |--------------------------------------------------------------------------
+        */
+
+        $twitterCard = trim(
+            $__env->yieldContent('twitter_card', 'summary_large_image')
+        );
+
+        $twitterUrl = trim(
+            $__env->yieldContent('twitter_url', $canonicalUrl)
+        );
+
+        $twitterTitle = trim(
+            $__env->yieldContent('twitter_title', $ogTitle)
+        );
+
+        $twitterDescription = trim(
+            $__env->yieldContent('twitter_description', $ogDescription)
+        );
+
+        $twitterImage = trim(
+            $__env->yieldContent('twitter_image', $sharedImage)
+        );
+
+        if (
+            $twitterImage &&
+            !str_starts_with($twitterImage, 'http://') &&
+            !str_starts_with($twitterImage, 'https://')
+        ) {
             $twitterImage = url('/' . ltrim($twitterImage, '/'));
         }
+
+        $twitterImageAlt = trim(
+            $__env->yieldContent('twitter_image_alt', $ogImageAlt)
+        );
     @endphp
 
     <title>{{ $title }}</title>
 
     <meta name="description" content="{{ $description }}">
 
-    @if ($keywords)
+    @if($keywords)
         <meta name="keywords" content="{{ $keywords }}">
     @endif
 
     <meta name="author" content="{{ $siteName }}">
     <meta name="robots" content="@yield('meta_robots', 'index, follow, max-image-preview:large')">
+
     <link rel="canonical" href="{{ $canonicalUrl }}">
 
     {{-- Open Graph --}}
     <meta property="og:locale" content="@yield('og_locale', 'en_NG')">
     <meta property="og:site_name" content="{{ $siteName }}">
     <meta property="og:type" content="{{ $ogType }}">
+    <meta property="og:url" content="{{ $ogUrl }}">
     <meta property="og:title" content="{{ $ogTitle }}">
     <meta property="og:description" content="{{ $ogDescription }}">
-    <meta property="og:url" content="{{ $canonicalUrl }}">
     <meta property="og:image" content="{{ $sharedImage }}">
 
-    @if (str_starts_with($sharedImage, 'https://'))
-        <meta property="og:image:secure_url" content="{{ $sharedImage }}">
+    @if($secureImage && str_starts_with($secureImage, 'https://'))
+        <meta property="og:image:secure_url" content="{{ $secureImage }}">
     @endif
 
-    @if ($ogImageType)
+    @if($ogImageType)
         <meta property="og:image:type" content="{{ $ogImageType }}">
     @endif
 
-    @if ($ogImageWidth)
+    @if($ogImageWidth)
         <meta property="og:image:width" content="{{ $ogImageWidth }}">
     @endif
 
-    @if ($ogImageHeight)
+    @if($ogImageHeight)
         <meta property="og:image:height" content="{{ $ogImageHeight }}">
     @endif
 
@@ -86,17 +183,18 @@
 
     {{-- Twitter / X --}}
     <meta name="twitter:card" content="{{ $twitterCard }}">
+    <meta name="twitter:url" content="{{ $twitterUrl }}">
     <meta name="twitter:title" content="{{ $twitterTitle }}">
     <meta name="twitter:description" content="{{ $twitterDescription }}">
     <meta name="twitter:image" content="{{ $twitterImage }}">
     <meta name="twitter:image:alt" content="{{ $twitterImageAlt }}">
 
     {{-- Favicon --}}
-    @if (setting('favicon'))
+    @if(setting('favicon'))
         <link rel="icon" type="image/png" href="{{ setting_asset(setting('favicon')) }}">
         <link rel="shortcut icon" href="{{ setting_asset(setting('favicon')) }}">
     @else
-        <link rel="icon" type="image/png" href="{{ asset('frontend/assets/images/favicon.png') }}">
+        <link rel="icon" type="image/png" href="{{ asset("frontend/images/favicon.png") }}">
     @endif
 
     @stack('meta')
@@ -106,7 +204,6 @@
     @stack('styles')
 
     {!! setting('header_scripts') !!}
-
 </head>
 
 <body id="bg" class="theme-rounded">
